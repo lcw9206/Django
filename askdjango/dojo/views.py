ecacf908587a1cc4991992e4f6c10628ac3169cc
Lee <lcw9206@gmail.com>
2018-02-08 15:02:15 +0900

    dojo.views.hello 한글 문자열 매칭

--- askdjango/dojo/views.py
+++ askdjango/dojo/views.py
@@ -6,3 +6,7 @@ def mysum(request, num):
     # result = sum(map(int, num.split("/")))    //와 같이 빈 문자열이 들어갔을 경우 에러 발생.
     result = sum(map(lambda s : int(s or 0), num.split('/')))       # s가 거짓일 때, or 뒤의 0의 값을 할당.
     return HttpResponse(result)
+
+def hello(request, name, age):
+
+    return HttpResponse("안녕하세요. {}님, {}살 이시네요.".format(name, age))
