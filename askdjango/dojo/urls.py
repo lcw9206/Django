1c2f0e57b08c432fca26f34c5a8055ad36f73906
Lee <lcw9206@gmail.com>
2018-02-08 14:53:08 +0900

    dojo앱에서 다수의 숫자 입력받아 더하기

--- askdjango/dojo/urls.py
+++ askdjango/dojo/urls.py
@@ -19,8 +19,6 @@ from django.contrib import admin
 from . import views
 
 urlpatterns = [
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)/(?P<z>\d+)$', views.mysum),
-    url(r'^sum/(?P<x>\d+)/(?P<y>\d+)$',views.mysum),
-    url(r'^sum/(?P<x>\d+)$',views.mysum),
+    url(r'^sum/(?P<num>[\d/]+)$',views.mysum),
 
 ]
--- askdjango/dojo/views.py
+++ askdjango/dojo/views.py
@@ -2,5 +2,7 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def mysum(request, x, y=0, z=0):
-    return HttpResponse(int(x) + int(y) + int(z))
+def mysum(request, num):
+    # result = sum(map(int, num.split("/")))    //와 같이 빈 문자열이 들어갔을 경우 에러 발생.
+    result = sum(map(lambda s : int(s or 0), num.split('/')))       # s가 거짓일 때, or 뒤의 0의 값을 할당.
+    return HttpResponse(result)
