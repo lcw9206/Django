b137a40efc4a342250c98ff59d3d6a13bebd7dbd
Lee <lcw9206@gmail.com>
2018-02-09 12:24:56 +0900

    blog.models.Post 모델 생성 및 마이그레이션

--- askdjango/blog/models.py
+++ askdjango/blog/models.py
@@ -1,3 +1,11 @@
+# blog/models.py
+
 from django.db import models
 
 # Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)    # auto_now_add : 최초 저장될 때, 일시가 저장된다.
+    updated_at = models.DateTimeField(auto_now=True)        # auto_now : 갱신될 때마다, 일시가 저장된다.
