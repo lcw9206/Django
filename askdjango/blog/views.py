412156bda22e46d5a2c7854e04a1fec48ead72cd
Lee <lcw9206@gmail.com>
2018-02-13 15:59:32 +0900

    blog.views.post_list 뷰에 QuerySet 검색 구현

--- askdjango/blog/views.py
+++ askdjango/blog/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
-
+from .models import Post
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html')
+    qs = Post.objects.all() # QuerySet을 가져왔으나 아직 DB에 적용되지는 않는다. 왜냐? 접근하지 않았기 때문.
+
+    q = request.GET.get('q', '')
+    if q:
+        qs = qs.filter(title__icontains=q)
+    return render(request, 'blog/post_list.html',{
+        'post_list' : qs,
+        'q' : q
+    })
