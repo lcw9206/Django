805ca97330ae432ee44bdf00b92b55d71ea581f8
Lee <lcw9206@gmail.com>
2018-03-15 22:41:46 +0900

    localhost:8000 접속 시, post_list로 접속 적용

--- askdjango/askdjango/urls.py
+++ askdjango/askdjango/urls.py
@@ -3,9 +3,11 @@
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf import settings
+from django.shortcuts import redirect
 
 
 urlpatterns = [
+    url(r'^$', lambda r: redirect('blog:post_list'), name='root'),      # redirect로 localhost:8000 접속 시, post_list로 토스
     url(r'^admin/', admin.site.urls),
     url(r'^blog/', include('blog.urls', namespace='blog')),
     url(r'^dojo/', include('dojo.urls', namespace='dojo')),
