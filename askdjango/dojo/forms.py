02b0b257ff0ced72bcb376b10539ef875bab98c7
Lee <lcw9206@gmail.com>
2018-03-27 16:59:47 +0900

    JavaScript로 dojo.models.Post.user_agent 필드값인 브라우저 정보를 hidden으로 채움

--- askdjango/dojo/forms.py
+++ askdjango/dojo/forms.py
@@ -30,7 +30,7 @@ class PostForm(forms.ModelForm):
 
     class Meta:
         model = Post
-        fields = ['title', 'content']   # 유저에게 입력받을 값, __all__ 설정은 전체 필드를 추가
+        fields = ['title', 'content','user_agent']   # 유저에게 입력받을 값, __all__ 설정은 전체 필드를 추가
         widgets = {
             'user_agent' : forms.HiddenInput,   # 가시적으로 노출되지는 않지만, input 박스가 존재한다.
         }
