edac7cc57b717cb29efcd25ad99320baa15010e4
Lee <lcw9206@gmail.com>
2018-03-28 17:34:51 +0900

    static finder 설정 및 static 샘플추가

--- askdjango/askdjango/settings.py
+++ askdjango/askdjango/settings.py
@@ -130,6 +130,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS =[
+    os.path.join(BASE_DIR, 'askdjango', 'static'),
+]
 
 INTERNAL_IPS = ["127.0.0.1"]
 
