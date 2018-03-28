54aa4a3ec6221a594d0ea0dd1bbf6d968ab49d09
Lee <lcw9206@gmail.com>
2018-03-28 15:41:26 +0900

    Admin 페이지 수정을 위한 dojo/admin.py 수정

--- askdjango/dojo/admin.py
+++ askdjango/dojo/admin.py
@@ -4,5 +4,14 @@ from django.contrib import admin
 from .models import Post, GameUser
 
 
-admin.site.register(Post)
+@admin.register(Post)   # 등록법 3 : 장식자 형태로 지원
+class PostAdmin(admin.ModelAdmin):
+    list_display = ['id', 'title', 'content_size', 'created_at', 'updated_at']
+    actions = ['make_published']
+
+    def content_size(self, post):
+        return '{}글자'.format(len(post.content))
+    content_size.short_description = '글자수'
+
+
 admin.site.register(GameUser)
