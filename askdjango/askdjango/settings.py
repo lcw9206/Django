49b47785965b96fd7c352c55482ee79aa82cc603
Lee <lcw9206@gmail.com>
2018-03-30 16:24:26 +0900

    템플릿에서 직접 이미지 썸네일 처리하기 및 약간의 수정

--- askdjango/askdjango/settings.py
+++ askdjango/askdjango/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django_extensions',
+    'imagekit',
     'bootstrap3',
     'debug_toolbar',
     'blog.apps.BlogConfig',
