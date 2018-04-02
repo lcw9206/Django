bf1b592367e014d8ec8dd527b731caf57cec5636
Lee <lcw9206@gmail.com>
2018-04-02 11:52:31 +0900

    CBV : ListView 적용

--- askdjango/blog/urls.py
+++ askdjango/blog/urls.py
@@ -3,13 +3,17 @@
 from django.conf.urls import url,include
 from django.contrib import admin
 from django.conf import settings
-from . import views
+from . import views, views_cbv
 
 
 urlpatterns = [
-    url(r'^$', views.post_list, name='post_list'),
+
+    #url(r'^$', views.post_list, name='post_list'),
     url(r'^(?P<id>\d+)/$', views.post_detail, name='post_detail'),
 
     url(r'^new/$', views.post_new, name='post_new'),
     url(r'^(?P<id>\d+)/edit/$', views.post_edit, name='post_edit'),
+
+    url(r'^cbv/new/$', views_cbv.post_new),
+    url(r'^$', views_cbv.post_list, name='post_list'),
 ]
---
+++ askdjango/blog/views_cbv.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.views.generic import ListView, CreateView
+from .models import Post
+
+
+post_list = ListView.as_view(model=Post, paginate_by=10)
+
+
+# blog/forms.py
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = '__all__'
+
+
+class PostCreateView(CreateView):
+    model = Post
+    form_class = PostForm
+
+
+post_new = PostCreateView.as_view()
