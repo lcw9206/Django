5a47c4171848267625110789a42252e4da8492b7
Lee <lcw9206@gmail.com>
2018-04-03 19:53:38 +0900

    layout 상속, 에러 관리 및 기타 수정

--- askdjango/accounts/forms.py
+++ askdjango/accounts/forms.py
@@ -1,6 +1,24 @@
-from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django import forms
+from .models import Profile
 
 
 class SignupForm(UserCreationForm):
+
+    phone_number = forms.CharField()
+
     class Meta(UserCreationForm.Meta):  # Meta를 상속받아 fields만 재정의
-        fields = UserCreationForm.Meta.fields + ('email', 'phone')
+        fields = UserCreationForm.Meta.fields + ('email',)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+
+        # 필수 필드에 생성되는 에러 생성 방지
+        self.fields['username'].required= False
+
+    def save(self):
+        user = super().save()
+        profile = Profile.objects.create(
+            user = user,
+            phone_number = self.cleaned_data['phone_number'])
+        return user
--- askdjango/accounts/urls.py
+++ askdjango/accounts/urls.py
@@ -8,9 +8,13 @@ from . import views
 
 urlpatterns = [
     url(r'^signup/$', views.signup, name='signup'),
+
+    # auth_views에 내장된 login 함수를 이용해 로그인을 처리
     url(r'^login/$', auth_views.login, name='login',
         kwargs={'template_name' : 'accounts/login_form.html'}),
+
     url(r'^logout/$', auth_views.logout, name='logout',
         kwargs={'next_page' : settings.LOGIN_URL}),
+
     url(r'^profile/$', views.profile, name='profile'),
 ]
